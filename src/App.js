d240067587d8778c50c91536ef3044f2608468e2
sitki.alagoz <sitki.alagoz@obss.com.tr>
2022-02-14 00:26:34 +0300

    Redirect property used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./page/Home";
 import About from "./page/About";
 import Comm from "./page/Comm";
@@ -9,10 +9,13 @@ function App() {
     <div className="App">
       <h1>Welcome to React Router!</h1>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="comm" element={<Comm />} />
-          <Route path="*" element={<div>merhaba</div>} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="comm" element={<Comm/>}/>
+        <Route
+          path="*"
+          element={<Navigate to="/"/>}
+        />
       </Routes>
     </div>
   );
